4ed7f9736476cf5a76cd1975f027f0c0f625e128
langstonBS <langston.beckwithstanley@gmail.com>
2020-03-27 13:17:36 -0700

    files layed out HTML poem decides

---
+++ app.js
@@ -0,0 +1,17 @@
+
+//Get dom elemtes
+
+//get html elments by id
+
+//get the inputs from te user
+
+//get input listners
+
+//get butten imputs
+
+//fuction that
+//get user ellenmtes
+//add elemets to the scrip poem
+//show the poem now changed 
+
+
